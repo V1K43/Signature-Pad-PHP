1056d84e2df4262e1008b55af36ddda196f86513
v1k45n <v1k45n@gmail.com>
2020-03-26 21:05:01 +0530

    first commit

---
+++ include/header.php
@@ -0,0 +1,38 @@
+		<header>
+			<style>
+				.navbar-default {
+					background: #fff;
+				}
+				ul.navbar-right li a:hover {
+					background: #518d8a !important;
+					color: #FFF !important;
+				}
+			</style>
+			
+			<nav class="navbar navbar-default">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="#" class="navbar-brand" style=" padding: 5px !important; margin-left: 30px !important; "> <img src="images/logo.png" class="site-logo" /></a>
+					</div>
+					<!-- Collection of nav links and other content for toggling -->
+					<div id="navbarCollapse" class="collapse navbar-collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+							   <a href="#" data-close="true"> Home </a>
+							</li>
+							<li>
+							   <a href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			
+		</header>
